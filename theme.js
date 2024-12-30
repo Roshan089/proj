bfa8dc139bde59bcaf52c7ceaca08b902a0cbc20
Roshan <1js20is089@gmail.com>
2024-12-30 10:00:36 +0530

    os

---
+++ theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // You can customize the primary color
+    },
+    secondary: {
+      main: '#dc004e', // You can customize the secondary color
+    },
+  },
+});
+
+export default theme;
